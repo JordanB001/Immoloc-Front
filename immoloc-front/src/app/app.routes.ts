db2dbc9f3a516681a9284e6ca5073cc45a49a892
Yassin Zairi <yassinzairi0@gmail.com>
2025-02-21 11:19:02 +0100

    update routing

--- immoloc-front/src/app/app.routes.ts
+++ immoloc-front/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component'
+import { AnalysePageComponent } from './pages/analyse-page/analyse-page.component'
+import { EstimationPageComponent } from './pages/estimation-page/estimation-page.component'
+import { GenerationPageComponent } from './pages/generation-page/generation-page.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'analyse', component: AnalysePageComponent },
+    { path: 'estimation', component: EstimationPageComponent },
+    { path: 'generation', component: GenerationPageComponent },
+    { path: '**', redirectTo: '' }
+];
